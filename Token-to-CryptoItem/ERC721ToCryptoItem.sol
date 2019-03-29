e5e6d7bd3a31c13e1be72629041a886f6b0fdcf6
Witek <witek@enjin.com>
2019-03-28 17:05:42 -0700

    token converter v2 with onERC721Received

--- Token-to-CryptoItem/ERC721ToCryptoItem.sol
+++ Token-to-CryptoItem/ERC721ToCryptoItem.sol
@@ -5,37 +5,77 @@ import "./ICryptoItems.sol";
 
 contract ERC721ToCryptoItem {
 
-    // Replace the following variables with the correct contracts and Token ID
-    address public erc721ContractAddress = 0x0;
-    address public erc1155ContractAddress = 0x0;
-    uint256 public erc721BurnAddress = 0x0000000000000000000000000000000000000000; // This can optionally be replaced with a smart contract address or recipient address if burning is not desired
-    uint256 public erc1155BaseId = 0x0;
-    address public previousCreator = 0x0;
+    bool public active = false;
+    address public originalCreator = 0x0;
+    address public erc721ContractAddress;
+    address public erc1155ContractAddress;
+    uint256 public erc1155BaseId;
 
-    event Convert(uint256 indexed _srcId, address indexed _destId);
+    // The burn address can be replaced with a real address if burning is not desired
+    uint256 public erc721BurnAddress = 0x0000000000000000000000000000000000000000;
 
-    function convert(uint256 _srcId) external returns (uint256 destId) {
-        if(previousCreator == 0x0) revert("Inactive");
+    // Equals to `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+    bytes4 constant private ERC721_RECEIVED = 0x150b7a02;
+
+    event Burn(address indexed from, uint256 indexed tokenId);
+
+    modifier originalCreatorOnly {
+        require(msg.sender == originalCreator);
+        _;
+    }
+
+    constructor(address memory _erc721ContractAddress, address memory _erc1155ContractAddress) public {
+        erc721ContractAddress = _erc721ContractAddress;
+        erc1155ContractAddress = _erc1155ContractAddress;
+    }
+
+    /*
+     * Burn the incoming ERC-721 token and mint an ERC-1155 token to the sender
+     */
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4) {
+        if(!active) revert("Inactive");
 
         // Transfer the ERC-721 token to the burn address
         ERC721 erc721Contract = ERC721(erc721ContractAddress);
-        erc721Contract.transferFrom(msg.sender, erc721BurnAddress, _srcId);
+
+        // This will revert if the token wasn't actually transferred over
+        // Note: If an unsafe transfer function was used, this is vulnerable to an attacker executing onERC721Received and claiming the ERC-1155 token
+        // This shouldn't happen under typical circumstances, though
+        erc721Contract.transferFrom(this, erc721BurnAddress, _tokenId);
+
 
         // Mint the ERC-1155 token to the sender's address
-        ERC721 erc1155Contract = ICryptoItems(erc1155ContractAddress);
-        erc1155Contract.mintNonFungible(erc1155BaseId, msg.sender);
+        ICryptoItems erc1155Contract = ICryptoItems(erc1155ContractAddress);
+        erc1155Contract.mintNonFungible(erc1155BaseId, _from);
 
         // Emit event signifying completion
-        emit Convert(_srcId, destId);
+        emit Burn(_from, _tokenId);
+
+        return ERC721_RECEIVED;
     }
 
-    function acceptAssignment() external {
-
-        previousCreator = msg.sender;
+    /*
+     * If unsafe transfer was used to send the ERC-721 token, the originalCreator can send it back.
+     */
+    function transferToken(uint256 _tokenId, address _to) originalCreatorOnly {
+        ERC721 erc721Contract = ERC721(erc721ContractAddress);
+        erc721Contract.transferFrom(this, _to, _tokenId);
     }
 
-    function assign() external {
+    function acceptAssignment(_id) external {
+        ICryptoItems erc1155Contract = ICryptoItems(erc1155ContractAddress);
+        erc1155Contract.acceptAssignment(_id);
 
-        previousCreator = 0x0;
+        erc1155BaseId = _id;
+        originalCreator = msg.sender;
+        active = true;
+    }
+
+    function assign() external originalCreatorOnly {
+        ICryptoItems erc1155Contract = ICryptoItems(erc1155ContractAddress);
+        erc1155Contract.assign(erc1155BaseId, originalCreator);
+
+        originalCreator = 0x0;
+        active = false;
     }
 }
